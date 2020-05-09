880b0d27faee06b2ec0a9696998af6d1eeee2c62
Neeraj Chouhan <>
2020-05-09 14:10:09 +0530

    Updates

---
+++ precache-manifest.336c543883a8dd6b7f39ce6d8ee0e3a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88384a1926050e9cefe16b0dc29941e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0004a205a3b13ff45725",
+    "url": "/static/css/2.b79fbc68.chunk.css"
+  },
+  {
+    "revision": "0fa06238d65232e7b8f6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0004a205a3b13ff45725",
+    "url": "/static/js/2.0eaacabd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0eaacabd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa06238d65232e7b8f6",
+    "url": "/static/js/main.3d725728.chunk.js"
+  },
+  {
+    "revision": "34d2e2b8771939c64f75",
+    "url": "/static/js/runtime-main.7e621c9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
