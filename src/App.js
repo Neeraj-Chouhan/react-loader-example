218a6b3d6e6f27359d8d85a7c46e6dae5ef100f2
Neeraj Chouhan <>
2020-05-09 14:08:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Loader } from 'simple-react-loader'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is simple react loader...</h1>
+      <Loader showLoader={true} />
     </div>
   );
 }
